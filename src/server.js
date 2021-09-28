1b20224626322d9b7bc75d1251a131dd6b5cc567
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-09-28 17:19:24 +0900

    work_9/28

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => res.render("home"));
+console.log("hello");
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
