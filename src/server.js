6acbc0fbb8b9f3fb784436d4aecb022790602c30
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-09-29 17:26:40 +0900

    work_9/29

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
-
+import WebSocket from "ws";
+import http from "http";
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.get("/", (req, res) => res.render("home"));
+app.set("view engine", "pug"); // 템플릿 렌더링, pug로 하겠다.
+app.set("views", __dirname + "/views"); // 디렉토리 위치 설정
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home")); // if (user => url) => (get req, res) and (render "home")
+app.get("/*", (_, res) => res.redirect("/"));
 console.log("hello");
-const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
