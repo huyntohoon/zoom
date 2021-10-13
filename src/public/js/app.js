26b3b815e16f17162a8bfc1b4d85cb5d07d2acc0
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-10-13 14:32:59 +0900

    done_10/13

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,49 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
-
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 let myStream;
+let muted = false;
+let cameraOff = false;
 
-function getMedia() {}
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    // user_usermedia => string
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getMedia();
+function handelMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handelCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera on";
+    cameraOff = true;
+  }
+}
+muteBtn.addEventListener("click", handelMuteClick);
+cameraBtn.addEventListener("click", handelCameraClick);
--- src/server.js
+++ src/server.js
@@ -14,3 +14,6 @@ app.get("/*", (_, res) => res.redirect("/")); // '/*' => everything => send '/'
 const httpServer = http.createServer(app); // http_Server make using express() moudule
 //const wss = new WebSocket.Server({ server }); // make WebSocket_Server on http server
 const wsServer = SocketIO(httpServer);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
